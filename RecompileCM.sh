d9075a43b0b80b592ed2e748c8c6133e0758a3f5
Fernando Oliveira <fernandoaju78@gmail.com>
2014-01-28 11:00:06 -0200

    Update RecompileCM.sh

--- RecompileCM.sh
+++ RecompileCM.sh
@@ -1,5 +1,5 @@
 #Compile
-. build/envsetup.sh && lunch cm_kumquat-userdebug && make -j6 otapackage;
+. build/envsetup.sh && brunch cm_kumquat-userdebug;
 
 #Beep (3x)
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga;
